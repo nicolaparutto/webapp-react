199c06fb6c91f3c7fa7a4f7e9d30bf92f70c6cf9
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-10 12:50:22 +0100

    fix: correct stars rating key

--- src/components/StarsRating.jsx
+++ src/components/StarsRating.jsx
@@ -4,12 +4,12 @@ function StarsRating(props) {
    let stars = [];
 
    for (let i = 0; i < 5; i++) {
-      stars.push(<i className={`fa-solid fa-star ${i < vote ? 'yellow' : 'grey'}-star`}></i>)
+      stars.push(<i key={i} className={`fa-solid fa-star ${i < vote ? 'yellow' : 'grey'}-star`}></i>)
    }
 
    return (
       <>
-         <div>{stars}</div >
+         <div>{stars}</div>
       </>
    )
 }

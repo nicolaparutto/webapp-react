3afca23eee068641629f6e941f7969566a813df2
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-10 13:04:44 +0100

    feat: add form layout in NewReviewPage

--- src/pages/NewReviewPage.jsx
+++ src/pages/NewReviewPage.jsx
@@ -1,6 +1,18 @@
 function NewReviewPage() {
    return (
-      <div>newReviewPage</div>
+      <section className="container">
+         <div className="new-review-container">
+            <form action="">
+               <label htmlFor="">Nome</label>
+               <input type="text" name="name" placeholder="Nome..." id="" />
+               <label htmlFor="">Testo</label>
+               <input type="text" name="text" placeholder="Testo..." id="" />
+               <label htmlFor="">Voto (Da 1 a 5)</label>
+               <input type="number" name="vote" placeholder="Voto..." id="" max={5} min={1} />
+               <button className="btn" type="submit">Aggiungi</button>
+            </form>
+         </div>
+      </section>
    )
 }
 

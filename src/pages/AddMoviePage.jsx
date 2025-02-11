3f2fd921c3d5644944af51a14cb67987679919b5
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-11 18:32:39 +0100

    fix: add several fixes that were causing the system to not work

--- src/pages/AddMoviePage.jsx
+++ src/pages/AddMoviePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { data, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function AddMoviePage() {
 
@@ -8,7 +8,7 @@ function AddMoviePage() {
       title: '',
       director: '',
       genre: '',
-      relase_year: '',
+      release_year: '',
       abstract: '',
       image: null
    }
@@ -30,16 +30,18 @@ function AddMoviePage() {
       }
    }
 
+   const defaultApiUrl = 'http://localhost:3000/api/movies';
+   const navigate = useNavigate();
    const handlerSubmit = (e) => {
       e.preventDefault();
+
       const dataToSend = new FormData();
       for (let key in newMovieData) {
          dataToSend.append(key, newMovieData[key]);
       }
 
-      const defaultApiUrl = 'http://localhost:3000/api/movies';
       axios.post(defaultApiUrl, dataToSend, { headers: { 'Content-Type': 'multipart/form-data' } })
-         .then(() => Navigate('/home'))
+         .then(() => navigate('/home'))
    }
 
    return (
@@ -47,22 +49,22 @@ function AddMoviePage() {
          <div className="new-movie-form">
             <form action="" onSubmit={handlerSubmit}>
 
-               <label htmlFor="title">Titolo</label>
+               <label htmlFor="for-title">Titolo</label>
                <input type="text" name="title" placeholder="Titolo..." value={newMovieData.title} onChange={changeInputValue} />
 
-               <label htmlFor="director">Regista cinematografico</label>
+               <label htmlFor="for-director">Regista cinematografico</label>
                <input type="text" name="director" placeholder="Regista..." value={newMovieData.director} onChange={changeInputValue} />
 
                <label htmlFor="genre">Genere</label>
                <input type="text" name="genre" placeholder="Genere..." value={newMovieData.genre} onChange={changeInputValue} />
 
-               <label htmlFor="year">Anno di rilascio</label>
-               <input type="number" name="relase_year" placeholder="Anno..." value={newMovieData.relase_year} onChange={changeInputValue} />
+               <label htmlFor="for-year">Anno di rilascio</label>
+               <input type="number" name="release_year" placeholder="Anno..." value={newMovieData.release_year} onChange={changeInputValue} />
 
-               <label htmlFor="description">Descrizione</label>
+               <label htmlFor="for-description">Descrizione</label>
                <textarea type="text" name="abstract" placeholder="Descrizione..." value={newMovieData.abstract} onChange={changeInputValue} />
 
-               <label htmlFor="image">Immagine</label>
+               <label htmlFor="for-image">Immagine</label>
                <input type="file" name="image" onChange={changeInputValue} />
 
                <button className="btn" type="submit">Aggiungi</button>
